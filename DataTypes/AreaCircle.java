f834a6cfd8c3d605b585c521c9a813ddf7fa54d5
Raunak0713 <raunakbhanarkar7@gmail.com>
2023-06-20 21:05:13 +0530

    f

--- DataTypes/AreaCircle.java
+++ DataTypes/AreaCircle.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class AreaCircle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int rad = sc.nextInt();
+        float rad = sc.nextInt();
         System.out.println(3.14*rad*rad);
         sc.close();
     }
