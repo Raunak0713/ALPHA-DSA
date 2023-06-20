db6f4e1060000d1dc0a35a69faa31a5bb7425bd0
Raunak0713 <raunakbhanarkar7@gmail.com>
2023-06-20 20:56:21 +0530

    i

---
+++ DataTypes/Input.java
@@ -0,0 +1,12 @@
+package DataTypes;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(s);
+        sc.close();
+    }
+}
