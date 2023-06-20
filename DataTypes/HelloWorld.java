2259dc8dfde57f938f16ecc29f9dc3ca56da25ef
Raunak0713 <raunakbhanarkar7@gmail.com>
2023-06-20 20:33:40 +0530

    first commit

---
+++ DataTypes/HelloWorld.java
@@ -0,0 +1,9 @@
+package DataTypes;
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Hello World");
+        System.out.println();
+    }
+}
