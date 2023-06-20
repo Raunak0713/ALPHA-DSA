277d223e56b92584636efb914683c3a649d0dcb9
Raunak0713 <raunakbhanarkar7@gmail.com>
2023-06-20 20:52:48 +0530

    s

--- DataTypes/Sum.java
+++ DataTypes/Sum.java
@@ -1,8 +1,9 @@
 package DataTypes;
 
-public class Variables {
+public class Sum {
     public static void main(String[] args) {
         int a = 10;
-        System.out.println(a);
+        int b = 20;
+        System.out.println(a+b);
     }
 }
