86a313557ed904054496c776be407f1cc6f0e778
Raunak0713 <raunakbhanarkar7@gmail.com>
2023-06-22 20:29:32 +0530

    unary

---
+++ Operators/Unary/Increment.java
@@ -0,0 +1,8 @@
+package Operators.Unary;
+
+public class Increment {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a++);
+    }
+}
---
+++ Operators/Unary/decrement.java
@@ -0,0 +1,8 @@
+package Operators.Unary;
+
+public class decrement {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a--);
+    }
+}
