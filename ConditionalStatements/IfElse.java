3fe001660777a471bc43e29aa6125a49e560d3b9
Raunak0713 <raunakbhanarkar7@gmail.com>
2023-06-25 10:48:33 +0530

    if

---
+++ ConditionalStatements/IfElse.java
@@ -0,0 +1,13 @@
+package ConditionalStatements;
+
+public class IfElse{
+    public static void main(String[] args) {
+        int age = 16;
+        if(age>17){
+            System.out.println("Adult");
+        }
+        else{
+            System.out.println("Not Adult");
+        }
+    }
+}
