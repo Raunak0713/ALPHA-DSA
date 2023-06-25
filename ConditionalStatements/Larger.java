f4faa2a89b362607786cb2e2c70e3d4525eddf4f
Raunak0713 <raunakbhanarkar7@gmail.com>
2023-06-25 10:50:27 +0530

    even

---
+++ ConditionalStatements/Larger.java
@@ -0,0 +1,9 @@
+package ConditionalStatements;
+
+public class Larger {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 45;
+        System.out.println(a>b ? a : b);
+    }
+}
---
+++ ConditionalStatements/OddEven.java
@@ -0,0 +1,8 @@
+package ConditionalStatements;
+
+public class OddEven {
+    public static void main(String[] args) {
+        int a = 13;
+        System.out.println(a%2==0 ? "even" : "odd");
+    }
+}
