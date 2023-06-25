643e663d7e1c567dfc7e1f467c8003e9b2613c6f
Raunak0713 <raunakbhanarkar7@gmail.com>
2023-06-25 11:01:45 +0530

    pass

---
+++ ConditionalStatements/PassFail.java
@@ -0,0 +1,8 @@
+package ConditionalStatements;
+
+public class PassFail {
+    public static void main(String[] args) {
+        int marks = 32;
+        System.out.println(marks>=33?"Pass":"Fail");
+    }
+}
